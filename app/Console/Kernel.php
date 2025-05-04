d88a7ef9940a1787b5d285aab6d658f2c7d10e68
Syed Azlan <syedazlan5169@gmail.com>
2025-05-04 22:45:39 +0800

    update console kernel and routes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,29 +7,8 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * These jobs run in a default, single-user context.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // Check for BL with Telex Release reminders every hour
-        $schedule->command('reminders:bl-telex-release')->everySixHours();
-    }
-
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
     }
 } 
