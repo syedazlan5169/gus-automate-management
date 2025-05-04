dec98bfdbe0e438252c7d3ba290c5a2d2394e37b
Syed Azlan <syedazlan5169@gmail.com>
2025-05-04 22:50:27 +0800

    update console kernel and routes

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-$schedule->command('reminders:bl-telex-release')->everySixHours();
+Schedule::command('reminders:bl-telex-release')->everySixHours();
